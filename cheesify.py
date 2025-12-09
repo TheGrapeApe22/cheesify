0296d844cdaaa6fdd45248ee062430aa7c588ff5
thegrapeape22 <rohan.nadkarni123@gmail.com>
2025-12-09 10:43:52 -0800

    print usage when args[1] is '' instead of just nonexistant

--- cheesify.py
+++ cheesify.py
@@ -4,7 +4,7 @@
 import re
 import sys
 
-if len(sys.argv) < 2 || sys.argv[1].size() == 0:
+if len(sys.argv) < 2 or len(sys.argv[1]) == 0:
     print("Usage: python3 cheesify.py [input file]")
     exit()
 
